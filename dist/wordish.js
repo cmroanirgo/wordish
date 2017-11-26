40ed387c37562c0f9247385f91dcbd3a80bc21ab
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-11-26 17:36:35 +1100

    fixed browser exports

--- dist/wordish.js
+++ dist/wordish.js
@@ -1,11 +1,22 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Wordish = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * @license GPLv3
+ * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
+ *
+ * This file is for browser consumption
+ **/
+
+window.Wordish = require('./index');
+
+
+},{"./index":2}],2:[function(require,module,exports){
 /**
  * @license GPLv3
  * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
  **/
 module.exports = require('./lib/dict');
 
-},{"./lib/dict":2}],2:[function(require,module,exports){
+},{"./lib/dict":3}],3:[function(require,module,exports){
 // A simple lookup system, recursively mapping:
 //  	a letter/character to it's usage
 //		a child 'dictionary' of characters that follow it.
@@ -306,5 +317,4 @@ Randomizer.prototype.generate = function(min, max) {
 
 module.exports = RootDict;
 
-},{"crypto":undefined}]},{},[1])(1)
-});
+},{"crypto":undefined}]},{},[1]);
---
+++ exports.js
@@ -0,0 +1,9 @@
+/**
+ * @license GPLv3
+ * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
+ *
+ * This file is for browser consumption
+ **/
+
+window.Wordish = require('./index');
+
