879abeda18ad7df76fd8a5555123e293bdbc169a
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-11-28 09:12:16 +1100

    Allow more properties to be obfuscated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * @license GPLv3
  * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
+ * Please refer to https://github.com/cmroanirgo/wordish for the licensing and unobfuscated source
  **/
 module.exports = require('./lib/dict');
