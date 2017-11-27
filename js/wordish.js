a0f4f731ecde9c645255f2d7837fcfabf7582367
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-11-28 09:41:11 +1100

    added tolstoy

--- js/wordish.js
+++ js/wordish.js
@@ -1,28 +1,28 @@
-(function t(e, r, i) {
+(function t(r, e, i) {
     function n(a, s) {
-        if (!r[a]) {
-            if (!e[a]) {
-                var h = typeof require == "function" && require;
-                if (!s && h) return h(a, !0);
+        if (!e[a]) {
+            if (!r[a]) {
+                var u = typeof require == "function" && require;
+                if (!s && u) return u(a, !0);
                 if (o) return o(a, !0);
-                var u = new Error("Cannot find module '" + a + "'");
-                throw u.code = "MODULE_NOT_FOUND", u;
+                var h = new Error("Cannot find module '" + a + "'");
+                throw h.code = "MODULE_NOT_FOUND", h;
             }
-            var f = r[a] = {
+            var f = e[a] = {
                 exports: {}
             };
-            e[a][0].call(f.exports, function(t) {
-                var r = e[a][1][t];
-                return n(r ? r : t);
-            }, f, f.exports, t, e, r, i);
+            r[a][0].call(f.exports, function(t) {
+                var e = r[a][1][t];
+                return n(e ? e : t);
+            }, f, f.exports, t, r, e, i);
         }
-        return r[a].exports;
+        return e[a].exports;
     }
     var o = typeof require == "function" && require;
     for (var a = 0; a < i.length; a++) n(i[a]);
     return n;
 })({
-    1: [ function(t, e, r) {
+    1: [ function(t, r, e) {
         /**
  * @license GPLv3
  * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
@@ -33,27 +33,28 @@
     }, {
         "./index": 2
     } ],
-    2: [ function(t, e, r) {
+    2: [ function(t, r, e) {
         /**
  * @license GPLv3
  * Copyright (c) 2017 Craig Monro (cmroanirgo). All rights reserved.
+ * Please refer to https://github.com/cmroanirgo/wordish for the licensing and unobfuscated source
  **/
-        e.exports = t("./lib/dict");
+        r.exports = t("./lib/dict");
     }, {
         "./lib/dict": 3
     } ],
-    3: [ function(t, e, r) {
+    3: [ function(t, r, e) {
         "use strict";
         var i = typeof window !== "undefined";
         if (!i) var n = t("crypto");
         function o(t) {
-            for (var e = 1; e < arguments.length; e++) {
-                var r = arguments[e];
-                if (r === null || typeof r !== "object") continue;
-                var i = Object.keys(r);
+            for (var r = 1; r < arguments.length; r++) {
+                var e = arguments[r];
+                if (e === null || typeof e !== "object") continue;
+                var i = Object.keys(e);
                 var n = i.length;
                 while (n--) {
-                    t[i[n]] = r[i[n]];
+                    t[i[n]] = e[i[n]];
                 }
             }
             return t;
@@ -66,119 +67,119 @@
             randomizer: new d()
         };
         function s() {
-            this.totalUsage = 0;
-            this.items = {};
-            this.depth = 0;
+            this.a = 0;
+            this.b = {};
+            this.c = 0;
         }
-        s.prototype.a = function(t) {
-            var e = this.items[t];
-            if (!e) {
-                e = new f(t);
-                this.items[t] = e;
-            }
-            e.usage++;
-            this.totalUsage++;
-            return e;
-        };
-        s.prototype.b = function(t, e) {
-            var r = this;
-            for (var i = 0; i < t.length; i++) {
-                var n = r.a(t[i]);
-                if (t[i] == " ") r = e; else if (r.depth >= e.maxDepth) {
-                    r = r.parent || e;
-                    if (i > 0) {
-                        var n = e.get(t[i]);
-                        if (n) r = n.c(e);
-                    }
-                } else r = n.c(r);
-            }
-        };
         s.prototype.d = function(t) {
-            t = Math.floor(t);
-            var e = 0;
-            var r = null;
-            for (var i in this.items) {
-                r = this.items[i];
-                e += r.usage;
-                if (t <= e) return r;
+            var r = this.b[t];
+            if (!r) {
+                r = new f(t);
+                this.b[t] = r;
             }
+            r.usage++;
+            this.a++;
             return r;
         };
-        s.prototype.get = function(t) {
-            return this.items[t];
+        s.prototype.e = function(t, r) {
+            var e = this;
+            for (var i = 0; i < t.length; i++) {
+                var n = e.d(t[i]);
+                if (t[i] == " ") e = r; else if (e.c >= r.f) {
+                    e = e.parent || r;
+                    if (i > 0) {
+                        var n = r.get(t[i]);
+                        if (n) e = n.g(r);
+                    }
+                } else e = n.g(e);
+            }
         };
-        s.prototype.e = function(t, e, r) {
-            var i = e.generate(0, this.totalUsage);
-            var n = this.d(i);
+        s.prototype.h = function(t) {
+            t = Math.floor(t);
+            var r = 0;
+            var e = null;
+            for (var i in this.b) {
+                e = this.b[i];
+                r += e.usage;
+                if (t <= r) return e;
+            }
+            return e;
+        };
+        s.prototype.get = function(t) {
+            return this.b[t];
+        };
+        s.prototype.i = function(t, r, e) {
+            var i = r.generate(0, this.a);
+            var n = this.h(i);
             if (!n) {
-                var o = r;
+                var o = e;
                 n = o.get(t);
                 if (!!n) {
-                    o = n.c(o);
-                    if (o == this) return r.e(t, e, r);
-                    return o.e(t, e, r);
+                    o = n.g(o);
+                    if (o == this) return e.i(t, r, e);
+                    return o.i(t, r, e);
                 }
                 return "";
             }
             if (!n || n.char == " ") return "";
-            var a = n.c(this).e(n.char, e, r);
+            var a = n.g(this).i(n.char, r, e);
             return n.char + a;
         };
-        function h(t) {
+        function u(t) {
             s.call(this);
-            this.maxDepth = t || 5;
+            this.f = t || 5;
         }
-        o(h.prototype, s.prototype);
-        h.prototype.reset = function() {
-            this.totalUsage = 0;
-            this.items = {};
+        o(u.prototype, s.prototype);
+        u.prototype.reset = function() {
+            this.a = 0;
+            this.b = {};
         };
-        h.prototype.createWord = function(t) {
+        u.prototype.createWord = function(t) {
             t = o({}, a, t);
             if (t.minWordLen >= t.maxWordLen) throw new Error("Minimum word length (" + t.minWordLen + ") should be shorter than the maximum word length (" + t.maxWordLen + ")");
-            var e;
+            var r;
             do {
-                e = this.e(" ", t.randomizer, this).trim();
-            } while (e.length < t.minWordLen || e.length > t.maxWordLen);
-            return e;
+                r = this.i(" ", t.randomizer, this).trim();
+            } while (r.length < t.minWordLen || r.length > t.maxWordLen);
+            return r;
         };
-        h.prototype.createWords = function(t) {
+        u.prototype.createWords = function(t) {
             t = o({}, a, t);
-            var e = [];
-            var r = t.numWords + (t.randomizeNumWords > 0 ? c(0, t.randomizeNumWords) : 0);
-            while (r-- > 0) {
+            var r = [];
+            var e = t.numWords + (t.randomizeNumWords > 0 ? c(0, t.randomizeNumWords) : 0);
+            while (e-- > 0) {
                 var i = 100;
                 do {
                     var n = this.createWord(t);
-                } while (e.indexOf(n) >= 0 && i-- > 0);
-                e.push(n);
+                } while (r.indexOf(n) >= 0 && i-- > 0);
+                r.push(n);
             }
-            return e;
+            return r;
         };
-        function u(t) {
+        function h(t) {
             var t = t.toLowerCase().replace(/[^a-z]/gi, " ").replace(/(?:and|an|or|the)/gi, " ").replace(/  /g, " ").trim();
             return t;
         }
-        h.prototype.learn = function(t, e) {
-            if (!e) e = u;
-            t = e.call(this, t);
-            this.b(t, this);
+        u.prototype.learn = function(t, r) {
+            if (!r) r = h;
+            t = r.call(this, t);
+            this.e(t, this);
         };
         function f(t) {
             this.char = t;
             this.usage = 0;
         }
-        f.prototype.c = function(t) {
-            if (!this.items) {
-                this.items = new s();
-                this.items.parent = t;
-                this.items.depth = t.depth + 1;
+        f.prototype.g = function(t) {
+            if (!this.b) {
+                this.b = new s();
+                this.b.parent = t;
+                this.b.c = t.c + 1;
             }
-            return this.items;
+            return this.b;
         };
-        function c(t, e) {
-            var r = new d(1);
-            return r.generate(t, e);
+        function c(t, r) {
+            var e = new d(1);
+            return e.generate(t, r);
         }
         function d(t) {
             this.rand = [];
@@ -192,19 +193,19 @@
                 this.values = n.randomBytes(t);
             }
         }
-        d.prototype.generate = function(t, e) {
+        d.prototype.generate = function(t, r) {
             if (this.at >= this.values.length) {
                 if (i) window.crypto.getRandomValues(this.values); else this.values = n.randomBytes(this.values.length);
                 this.at = 0;
             }
-            var r = this.values[this.at++] | this.values[this.at++] << 8 | this.values[this.at++] << 16 | this.values[this.at++] << 24;
-            r = r >>> 0;
+            var e = this.values[this.at++] | this.values[this.at++] << 8 | this.values[this.at++] << 16 | this.values[this.at++] << 24;
+            e = e >>> 0;
             var o = 4294967295 >>> 0;
-            r = r * (e - t) / o + t;
-            if (r > e) r = e;
-            return r;
+            e = e * (r - t) / o + t;
+            if (e > r) e = r;
+            return e;
         };
-        e.exports = h;
+        r.exports = u;
     }, {
         crypto: undefined
     } ]
